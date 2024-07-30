4d0f0258539614a930d3960d3b850427c3f1119b
simon <simonchasnovsky@gmail.com>
2024-07-30 13:37:37 -0300

    Add dynamic favicon

--- app/Templates/Layouts/Components/head.php
+++ app/Templates/Layouts/Components/head.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Blog">
 
-  <link rel="icon" type="image/x-icon" href="imgs/favicon.png">
+  <link rel="icon" type="image/x-icon" href="imgs/blog/favicon.png">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
