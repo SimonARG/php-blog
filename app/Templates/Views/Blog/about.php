a0d69c3055281f574129583956ccd21ff9ff838c
simon <simonchasnovsky@gmail.com>
2024-08-07 21:11:34 -0300

    Fix about edit button showing up for non-admins

--- app/Templates/Views/Blog/about.php
+++ app/Templates/Views/Blog/about.php
@@ -2,19 +2,21 @@
     <div>
         <div class="body body-preview"><?= $blogConfig['info'] ?></div>
         <hr>
-        <span class="material-symbols-rounded btn edit">edit_square</span>
+        <?php if ($_SESSION && $_SESSION['role'] == 'admin'): ?>
+            <span class="material-symbols-rounded btn edit">edit_square</span>
+        <?php endif ?>
     </div>
 
-    <form action="/admin/about/update" method="POST">
+    <?php if ($_SESSION && $_SESSION['role'] == 'admin'): ?>
+        <form action="/admin/about/update" method="POST">
 
-        <textarea name="about" id="about"><?= $blogInfoRaw ?></textarea>
+            <textarea name="about" id="about"><?= $blogInfoRaw ?></textarea>
 
-        <div>
-            <button class="btn">Cancelar</button>
+            <div>
+                <button class="btn">Cancelar</button>
 
-            <?php if ($_SESSION && $_SESSION['role'] == 'admin'): ?>
                 <input class="btn" type="submit" value="Editar">
-            <?php endif; ?>
-        </div>
-    </form>
+            </div>
+        </form>
+    <?php endif; ?>
 </div>
